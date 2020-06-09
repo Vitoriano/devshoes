151bfe17605a82c5115a6b7ccfae1a466592a204
Vitoriano <vitorianoernandes2012@gmail.com>
2020-06-09 14:47:58 -0300

    estilos globais

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStayle  from './styles/global';
 import Routes from './routes';
+
 
 function App() {
   return (
     <BrowserRouter>
       {/* <Header /> */}
       <Routes />
+      <GlobalStayle />
     </BrowserRouter>
   );
 }
