7ce0918e38406ac5596d37e8b80b9d8800169dc1
Vitoriano <vitorianoernandes2012@gmail.com>
2020-06-16 16:19:19 -0300

    reactotron and configurando o redux nas rotas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
 
   state = {
     prodcuts: [],
@@ -22,6 +23,15 @@ export default class Home extends Component {
     this.setState({ prodcuts: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  }
+
   render() {
     const { prodcuts } = this.state;
 
@@ -33,7 +43,7 @@ export default class Home extends Component {
             <strong>{prodcut.title}</strong>
             <span>{prodcut.priceFormat}</span>
 
-            <button type="button">
+            <button type="button" onClick={() => this.handleAddProduct(prodcut)} >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -47,3 +57,4 @@ export default class Home extends Component {
   }
 }
 
+export default connect()(Home);
